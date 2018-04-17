d79ea6f7a0270d981066664710e02064a5b3ca40
Ritwick Dey <ritwickdey@outlook.com>
2018-04-17 23:18:27 +0530

    animation improved

--- src/app/navigator/navigator.component.ts
+++ src/app/navigator/navigator.component.ts
@@ -13,7 +13,9 @@ import {
   group,
   query,
   animate,
-  state
+  stagger,
+  state,
+  keyframes
 } from '@angular/animations';
 
 @Component({
@@ -22,8 +24,48 @@ import {
   styleUrls: ['./navigator.component.scss'],
   animations: [
     trigger('slideIn', [
-      state('void', style({ marginLeft: '50%', opacity: 0.1 })),
-      transition('void <=> *', animate('0.5s ease-in-out'))
+      transition('* => *', [
+        query(':enter', style({ marginLeft: '50%', opacity: 0 }), {
+          optional: true
+        }),
+        query(':leave', style({ marginLeft: '0%', opacity: 1 }), {
+          optional: true
+        }),
+        query(
+          ':enter',
+          [
+            stagger('0.3s', [
+              animate(
+                '1s ease-out',
+                keyframes([
+                  style({ offset: 0 }),
+                  style({ marginLeft: '0%', opacity: 0.8, offset: 0.8 }),
+                  style({ marginLeft: '-10%', opacity: 1, offset: 0.9 }),
+                  style({ marginLeft: '0%', opacity: 1, offset: 1 })
+                ])
+              )
+            ])
+          ],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+          [
+            stagger('0.3s', [
+              animate(
+                '1s ease-in',
+                keyframes([
+                  style({ offset: 0 }),
+                  style({ marginLeft: '10%', offset: 0.1 }),
+                  style({ marginLeft: '0%', offset: 0.4 }),
+                  style({ marginLeft: '-50%', opacity: 0, offset: 1 })
+                ])
+              )
+            ])
+          ],
+          { optional: true }
+        )
+      ])
     ])
   ]
 })
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, query, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  animations: [
+    trigger('animate', [
+      transition('* => *', [
+        query(':leave, :enter', animateChild(), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class ProfileComponent implements OnInit {
   showNavigationMenu = false;
