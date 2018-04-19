dda21ba65694328f50f85df9ffaa53932592c2fa
Ritwick Dey <ritwickdey@outlook.com>
2018-04-19 19:33:11 +0530

    animation in project card

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,73 @@
+import { Component, OnInit, EventEmitter, OnDestroy } from '@angular/core';
+import {
+  transition,
+  trigger,
+  query as q,
+  animateChild,
+  style,
+  group,
+  animate,
+  sequence,
+  stagger,
+  keyframes
+} from '@angular/animations';
+
+const query = (s, a, o = { optional: true }) => q(s, a, o);
+
+import { IProject } from '../utils/IProject';
 import { TagsService } from './../services/tags.service';
 import { ProjectDataService } from './../services/project-data.service';
-import { Component, OnInit, EventEmitter, OnDestroy } from '@angular/core';
-import { IProject } from '../utils/IProject';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      transition('* => *', [
+        query(':enter', style({ transform: 'translateY(-25%)', opacity: 0 }), {
+          optional: true
+        }),
+        query(':leave', style({ transform: 'translateY(0%)', opacity: 1 }), {
+          optional: true
+        }),
+        query(
+          ':enter',
+          [
+            stagger('0.2s', [
+              animate(
+                '0.3s ease-out',
+                keyframes([
+                  style({
+                    transform: 'translateY(-25%)',
+                    opacity: 0,
+                    offset: 0
+                  }),
+                  style({ transform: 'translateY(0%)', opacity: 1, offset: 1 })
+                ])
+              )
+            ])
+          ],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+          [
+            stagger('0.2s', [
+              animate(
+                '0.3s ease-in',
+                keyframes([
+                  style({ transform: 'translateY(0%)', offset: 0 }),
+                  style({ transform: 'translateY(25%)', opacity: 0, offset: 1 })
+                ])
+              )
+            ])
+          ],
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class ProjectsComponent implements OnInit, OnDestroy {
   projects: IProject[];
@@ -20,12 +81,16 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.renderProjects();
     this.tagSubscription$ = this.tagsService.onTagUpdate;
-    this.tagSubscription$.subscribe(() => this.renderProjects());
+    this.tagSubscription$.subscribe(() => {
+      if (this.projects.length === 0) this.renderProjects();
+      else this.projects = [];
+    });
   }
 
   ngOnDestroy() {
     if (this.tagSubscription$) this.tagSubscription$.unsubscribe();
   }
+
   renderProjects() {
     this.projects = this.projectService
       .getProjects()
--- src/app/services/tags.service.ts
+++ src/app/services/tags.service.ts
@@ -14,7 +14,14 @@ export class TagsService {
   }
 
   toogleTagSelection(tagName: string) {
+    const totalSelection = this.tags.reduce(
+      (prev, current) => prev + (current.isSelected ? 1 : 0),
+      0
+    );
     const selectedSkill = this.tags.find(e => e.displayName === tagName);
+
+    if (totalSelection <= 1 && selectedSkill.isSelected) return;
+
     selectedSkill.isSelected = !selectedSkill.isSelected;
     this.onTagUpdate.emit({
       tags: this.tags.filter(e => e.isSelected)
